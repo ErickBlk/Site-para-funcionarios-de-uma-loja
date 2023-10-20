691f88f35616f01bb8f2ba52b657a1a98e0cbdf2
ErickBlk <127314695+ErickBlk@users.noreply.github.com>
2023-10-20 23:29:06 +0100

    Novas Edições
    alterações na forma de mostrar os links diminuindo o tempo de resposta e deixando a interação com o usuario mais rapida, gerando menos tempo de acesso e mais fácil acesso ao serviço desejado, incluindo novo designer no site como açoes de hover e action

---
+++ prescript.js
@@ -0,0 +1,166 @@
+    
+function agua() {
+        var  tele = document.getElementById("ttelefonia")
+
+      tele.innerHTML = ''
+ var  ipptu = document.getElementById("iiptu")
+
+      ipptu.innerHTML = ''
+var  certidao = document.getElementById("ccertidoes")
+
+      certidao.innerHTML = ''
+ var  agenda = document.getElementById("aagendamento")
+
+      agenda.innerHTML = ''
+var  detran = document.getElementById("ddetran")
+
+      detran.innerHTML = ''
+ var  Bbancario = document.getElementById("bbancario")
+
+                Bbancario.innerHTML = ''
+
+        var aguaDiv = document.getElementById("agua-luz");
+                aguaDiv.innerHTML = '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Copasa-MG</a>  | ' +
+        '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Cemig-MG</a>  | ' + 
+        '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Cedae-RJ</a>  | ' + 
+        '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Embasa-BA</a>  | ' + 
+        '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Enel-RJ</a>'
+      }
+function bancario() {
+        var  tele = document.getElementById("ttelefonia")
+
+      tele.innerHTML = ''
+ var  ipptu = document.getElementById("iiptu")
+
+      ipptu.innerHTML = ''
+var  certidao = document.getElementById("ccertidoes")
+
+      certidao.innerHTML = ''
+ var  agenda = document.getElementById("aagendamento")
+
+      agenda.innerHTML = ''
+var  detran = document.getElementById("ddetran")
+
+      detran.innerHTML = ''
+ var aguaDiv = document.getElementById("agua-luz");
+                aguaDiv.innerHTML = ''
+        var  Bbancario = document.getElementById("bbancario")
+
+                Bbancario.innerHTML = '<a href="https://www63.bb.com.br/portalbb/boleto/boletos/hc21e,802,3322,10343.bbx?_ga=2.143951093.1302355173.1539000900-1597086691.1520263290">Banco do Brasil</a>  | ' + 
+                '<a href="https://banco.bradesco/html/classic/produtos-servicos/mais-produtos-servicos/segunda-via-boleto.shtm">Bradesco</a>  | ' +
+                '<a href="https://www.itau.com.br/servicos/boletos">Itau</a>  | ' + 
+                '<a href="https://www.santander.com.br/2-via-boleto">Santander</a>  | ' + 
+                '<a href="https://www.sicoob.com.br/web/sicoob/segunda-via-de-boleto">Sicoob</a>  | ' +
+                '<a href="https://mercantildobrasil.com.br/Servicos/Paginas/Servicos.aspx?Page=segunda-via-boleto">Mercantil</a>  | ' +
+                '<a href="https://www.original.com.br/boletooriginal/">Original</a>  | ' +
+                '<a href="https://www.bv.com.br/boleto">Banco-Bv</a> '
+      }
+function telefonia() {
+        
+   var  ipptu = document.getElementById("iiptu")
+  
+        ipptu.innerHTML = ''
+  var  certidao = document.getElementById("ccertidoes")
+  
+        certidao.innerHTML = ''
+   var  agenda = document.getElementById("aagendamento")
+  
+        agenda.innerHTML = ''
+  var  detran = document.getElementById("ddetran")
+  
+        detran.innerHTML = ''
+   var  Bbancario = document.getElementById("bbancario")
+  
+                  Bbancario.innerHTML = ''
+  var aguaDiv = document.getElementById("agua-luz");
+                  aguaDiv.innerHTML = ''
+
+
+
+        var  tele = document.getElementById("ttelefonia")
+
+      tele.innerHTML = '<a href="https://meutim.tim.com.br/novo/login?bmctx=8B94D49C137CCFE1FA9512A461AC79DB4FBA69EFB5FA39109654FE9A6E39100A&contextType=external&username=string&enablePersistentLogin=true&contextValue=%2Foam&password=secure_string&challenge_url=https%3A%2F%2Fmeutim.tim.com.br%2Fnovo%2Flogin&request_id=4007559052985068436&authn_try_count=0&locale=pt_BR&resource_url=https%253A%252F%252Fmeutim.tim.com.br%252F">Tim</a>  | ' + 
+
+      '<a href="https://login.vivo.com.br/loginmarca/appmanager/marca/publicoNovoLogin">Vivo</a>  | ' + 
+      
+      '<a href="https://www.claro.com.br/minha-claro">Claro</a>  | ' + 
+      
+      '<a href="https://www.oi.com.br/minha-oi/">Oi</a>  | ' + 
+      
+      '<a href="https://sac.blinktelecom.com.br/Login">Blink</a>  | ' + 
+      
+      '<a href="https://central.sempre.hubsoft.com.br/central/login">Sempre-Internet</a>' 
+      }
+function iptu() {
+        var  tele = document.getElementById("ttelefonia")
+
+        tele.innerHTML = ''
+   
+  var  certidao = document.getElementById("ccertidoes")
+  
+        certidao.innerHTML = ''
+   var  agenda = document.getElementById("aagendamento")
+  
+        agenda.innerHTML = ''
+  var  detran = document.getElementById("ddetran")
+  
+        detran.innerHTML = ''
+   var  Bbancario = document.getElementById("bbancario")
+  
+                  Bbancario.innerHTML = ''
+  var aguaDiv = document.getElementById("agua-luz");
+                  aguaDiv.innerHTML = ''
+       
+       
+        var  ipptu = document.getElementById("iiptu")
+
+      ipptu.innerHTML = '<a href="https://copasaportalprd.azurewebsites.net/Copasa.Portal/Login/Index">Betim</a>  | ' + 
+      
+      '<a href="https://iptu.pbh.gov.br/IptuOnline/index.xhtml">Belo-Horizonte</a>  | ' + 
+      
+      '<a href="http://receita.contagem.mg.gov.br/iptu2023/guia/">Contagem</a>  | ' + 
+      
+      '<a href="https://pmibirite.geosiap.net.br/pmibirite/websis/siapegov/arrecadacao/2via/index.php">Ibirite</a>  | ' + 
+      
+      '<a href="https://sistemas.saojoaquimdebicas.mg.gov.br/portalcidadao/#78c3e513dd43cb27d8a3e2f376196ffc656d7ea577b2c6fb0e24ec1f2f3a01150c8035be804cea051f7e280bf899c4c43ec632b779c79b71dfad3ec558c5003864dfbce426fe691d64cdc3f253325d0f120d6fafd3b58d28d34a16b15af4665c83f3491e1c46ef22edec8ec6e345bd86543bde2b4361357f16dfa0c6d37009faaa00881c8dea520f735c32e0b7716f1b9f630e1e67167e7e322f153e1a41d513d60b56c213afcd37bc7cc5290f7cbff24c231189de0bdd50610e62ab79001821bcbbfcb7b47e18e924ef2a6919e8914f2196282526649d38">São Joaquim de Bicas</a>  | ' + 
+      
+      '<a href="https://sistemas.igarape.mg.gov.br/portalcidadao/#075f539f0b7223f116d2c85c4ce1b1752fccb0db1fd92284312b33310fb199ef6050e9373e0f36365cbb7737a0e49e582e657146a648fd13d54aa9e4338df879e807578fb1eeafd7b9e70283f841665c4a3e02fd4fbc9800327395f2d5960e13edf4eef92f7be48b602f270581235aa52dab955a3b45972e">Igarapé</a>  | ' + 
+      
+      '<a href="https://e-gov.betha.com.br/cdweb/03114-314/main.faces;jsessionid=B1A5D6B24CFAE7AB8564EE9E28F48897.p1node1">Ribeirão das Neves</a>  | ' + 
+      
+      '<a href="https://e-gov.betha.com.br/cdweb/03114-314/main.faces;jsessionid=B1A5D6B24CFAE7AB8564EE9E28F48897.p1node1">Sarzedo</a>'
+      }
+function certidao() {
+        var  tele = document.getElementById("ttelefonia")
+
+        tele.innerHTML = ''
+   var  ipptu = document.getElementById("iiptu")
+  
+        ipptu.innerHTML = ''
+  
+   var  agenda = document.getElementById("aagendamento")
+  
+        agenda.innerHTML = ''
+  var  detran = document.getElementById("ddetran")
+  
+        detran.innerHTML = ''
+   var  Bbancario = document.getElementById("bbancario")
+  
+                  Bbancario.innerHTML = ''
+  var aguaDiv = document.getElementById("agua-luz");
+                  aguaDiv.innerHTML = ''
+       
+       
+        var  certidao = document.getElementById("ccertidoes")
+
+      certidao.innerHTML = '<a href="https://wwws.pc.mg.gov.br/atestado/solicitarsel.do?evento=x&fwPlc=s">Atestado de Antecedentes Criminais MG</a>  | ' + 
+
+      '<a href="https://sistemas.trf1.jus.br/certidao/#/solicitacao">Certidão TRF1 Criminal/Cível</a>  | ' + 
+      
+      '<a href="https://www.tse.jus.br/servicos-eleitorais/certidoes/certidao-de-quitacao-eleitoral">Certidão de Quitação Eleitoral</a>  | ' + 
+      
+      '<a href="https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp">Regularidade CPF</a>  | ' + 
+      
+      '<a href="https://sistemas.trf1.jus.br/certidao/#/solicitacao">Certidão TJMG Criminal/Cível</a>'
+      
+      }
